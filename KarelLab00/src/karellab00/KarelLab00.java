e193cb93b45e20aa99f50d0873f38444df88ab56
Doug Heynderickx <doug_heynderickx@asl.org>
2014-11-18 08:34:25 +0000

    Work on Lab00

---
+++ KarelLab00/src/karellab00/KarelLab00.java
@@ -0,0 +1,31 @@
+/*
+ * Swag License 
+ */
+package karellab00;
+/**
+ * @author heynderickxd
+ */
+
+import edu.fcps.karel2.Display; 
+import edu.fcps.karel2.Robot;
+
+public class KarelLab00 {
+    public static void main(String[] args) {
+     Display.openWorld("maps/first.map");
+     Display.setSize(10, 10);
+     Display.setSpeed(5);
+     
+     Robot joe = new Robot();
+     
+     joe.move();
+     joe.pickBeeper();
+     joe.move();
+     joe.turnLeft();
+     joe.move();
+     joe.putBeeper();
+     joe.move();
+     joe.turnLeft();
+     joe.turnLeft();
+    }
+    
+}
