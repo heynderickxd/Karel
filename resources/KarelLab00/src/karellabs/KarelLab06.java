2dd68c7216b87f019ede70843daea1f428428518
Doug Heynderickx <doug_heynderickx@asl.org>
2014-11-24 19:24:46 +0000

    Finished Lab_06 Extra Tasks
    Need to test with more maps as well.

--- resources/KarelLab00/src/karellabs/KarelLab06.java
+++ resources/KarelLab00/src/karellabs/KarelLab06.java
@@ -8,14 +8,17 @@
          String filename = JOptionPane.showInputDialog("What robot world?");
          Display.openWorld("../maps/" + filename + ".map");
          Display.setSize(10, 10);
-         Display.setSpeed(4);
+         Display.setSpeed(10);
       
          task_01(); //go to the end of the row of beepers
          task_02(); //go to the beeper
          task_03(); //go to the wall
          task_04(); //go to the wall, pick up all the beepers (max one per pile)
          task_05(); //go to the wall, pick up all the beepers
-         task_06(); //go to the end of the row of beepers, there is one gap
+         task_06();//go to the end of the row of beepers, there is one gap
+         task_07();
+         task_08();
+         task_09();
       }
        public static void task_01()	
       { //go to the end of the row of beepers
@@ -31,8 +34,6 @@
          Robot temp = new Robot(1, 2, Display.EAST, 0);
          if(temp.frontIsClear() && !temp.nextToABeeper()){
              temp.move();
-         } else{
-             temp.pickBeeper();
          }
       }
        public static void task_03()	
@@ -45,7 +46,6 @@
        public static void task_04()	
       { //go to the wall, pick up all the beepers (max one per pile)
          Robot temp = new Robot(1, 4, Display.EAST, 0);
-         temp.pickBeeper();
          while(temp.frontIsClear()){
              if(temp.nextToABeeper()){
                  temp.pickBeeper();
@@ -79,7 +79,6 @@
          int c = temp.getX();
          while (temp.frontIsClear() && c < 10) {
             if(temp.nextToABeeper()){
-                 temp.pickBeeper();
                  temp.move();
                  c++;
              } else { 
@@ -88,4 +87,51 @@
              }
         }
       }
-   }
+       public static void task_07() {
+           Robot temp = new Robot(1,7,Display.EAST,0);
+           while(temp.frontIsClear() || !temp.nextToABeeper()){
+               temp.move();
+           }
+       }
+       public static void task_08() {
+           Robot temp2 = new Robot(9,8,Display.NORTH,0);
+           Robot temp = new Robot(1,8,Display.EAST,1); 
+           if(!temp.nextToARobot()){
+               temp.move();
+           }
+           temp.putBeeper();
+         }
+       public static void task_09(){
+           Robot temp = new Robot(1,9,Display.EAST,20);
+           int c = temp.getBeepers();
+           while (c>0){
+           for(int i= 3;i >= 0; --i){
+           temp.putBeeper();
+           }
+           temp.move();
+       }
+       }
+       public static void task_10(){
+            Robot temp = new Robot(1,10,Display.EAST,20);
+           while(temp.frontIsClear()){
+               while(temp.nextToABeeper()){
+                temp.move();
+               }
+               temp.putBeeper();
+           }
+       }
+       public static void task_11(){
+            Robot temp = new Robot(1,9,Display.EAST,20);
+            while(!temp.rightIsClear()){
+                temp.putBeeper();
+                temp.move();
+            }
+       }
+       public static void task_12(){
+            Robot temp = new Robot(1,9,Display.EAST,20);
+            while(!temp.rightIsClear() && !temp.nextToABeeper()){
+                temp.move();
+            }
+       }
+    }
+
