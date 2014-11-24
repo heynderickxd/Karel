bca063d7a5942ec786149fc7136a1d8caa7d7304
Doug Heynderickx <doug_heynderickx@asl.org>
2014-11-24 09:17:49 +0000

    Finished Lab_06

--- resources/KarelLab00/src/karellabs/KarelLab06.java
+++ resources/KarelLab00/src/karellabs/KarelLab06.java
@@ -6,9 +6,9 @@
        public static void main(String[] args) 
       {
          String filename = JOptionPane.showInputDialog("What robot world?");
-         Display.openWorld("maps/" + filename + ".map");
+         Display.openWorld("../maps/" + filename + ".map");
          Display.setSize(10, 10);
-         Display.setSpeed(10);
+         Display.setSpeed(4);
       
          task_01(); //go to the end of the row of beepers
          task_02(); //go to the beeper
@@ -20,7 +20,8 @@
        public static void task_01()	
       { //go to the end of the row of beepers
          Robot temp = new Robot(1, 1, Display.EAST, 0);
-         while(temp.frontIsClear()){
+         while(temp.frontIsClear() && temp.nextToABeeper()){
+             temp.move();
          }
          }
       
@@ -28,26 +29,60 @@
        public static void task_02()	
       { //go to the beeper
          Robot temp = new Robot(1, 2, Display.EAST, 0);
-      	
+         while(temp.frontIsClear() && !temp.nextToABeeper()){
+             temp.move();
+         }
       }
        public static void task_03()	
       { //go to the wall
          Robot temp = new Robot(1, 3, Display.EAST, 0);
-       
+         while(temp.frontIsClear()){
+             temp.move();
+         }
       }
        public static void task_04()	
       { //go to the wall, pick up all the beepers (max one per pile)
          Robot temp = new Robot(1, 4, Display.EAST, 0);
-      
+          while(temp.frontIsClear()){
+             if(temp.nextToABeeper()){
+                 temp.pickBeeper();
+             }
+              temp.move();
+             if(temp.nextToABeeper()){
+                 temp.pickBeeper();
+             }
+          }
       }
        public static void task_05()	
       { //go to the wall, pick up all the beepers
          Robot temp = new Robot(1, 5, Display.EAST, 0);
-      
+         while (temp.frontIsClear()){
+             if(temp.nextToABeeper()){
+                 temp.pickBeeper();
+             } else {
+                 temp.move();
+             }
+         }
+         while(!temp.frontIsClear()){
+         while (temp.nextToABeeper()) {
+             temp.pickBeeper();
+         }
+         return;
+             }
       }
        public static void task_06()
       { //go to the end of the row of beepers, there is one gap
          Robot temp = new Robot(1, 6, Display.EAST, 0);
-      
+         int c = temp.getX();
+         while (temp.frontIsClear() && c < 10) {
+            if(temp.nextToABeeper()){
+                 temp.pickBeeper();
+                 temp.move();
+                 c++;
+             } else { 
+                 temp.move();
+                 c++;
+             }
+        }
       }
    }
