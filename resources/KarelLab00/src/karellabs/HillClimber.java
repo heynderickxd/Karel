6e598133652237e3eb404b2cb34a583c488c8776
Doug Heynderickx <doug_heynderickx@asl.org>
2014-12-01 11:18:51 +0000

    Started work on Lab 07

---
+++ resources/KarelLab00/src/karellabs/HillClimber.java
@@ -0,0 +1,27 @@
+package karellabs;
+/**
+ * @author heynderickxd
+ */
+import edu.fcps.karel2.Display;
+
+public class HillClimber extends Climber {
+
+	public HillClimber(int x) {
+		//TODO Call the appropriate superclass constructor
+	}
+	
+	public void climbUpRight() {
+		//TODO Climb up one and across two
+        }
+	public void climbUpLeft() {
+		//TODO Climb up one and across two
+	}
+	
+	public void climbDownRight() {
+		//TODO Climb down one and across two
+	}
+	
+	public void climbDownLeft() {
+		//TODO Climb down one and across two
+	}
+}
---
+++ resources/KarelLab00/src/karellabs/StepClimber.java
@@ -0,0 +1,28 @@
+package karellabs;
+/**
+ * @author heynderickxd
+ */
+import edu.fcps.karel2.Display;
+
+public class StepClimber extends Climber {
+
+	public StepClimber(int x) {
+		//TODO Call the appropriate superclass constructor
+	}
+	
+	public void climbUpRight() {
+		//TODO Climb one step up and one across
+	}
+	
+	public void climbUpLeft() {
+		//TODO Climb one step up and one across
+	}
+	
+	public void climbDownRight() {
+		//TODO Climb one step down and one across
+	}
+	
+	public void climbDownLeft() {
+		//TODO Climb one step down and one across
+	}
+}
